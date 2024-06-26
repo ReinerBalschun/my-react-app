37dbe28fe35a04ab9b1d7996765c82c294aa4dd1
ReinerBalschun <reinermbalschun@gmail.com>
2024-06-26 23:36:13 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx';
+import Food from './Food.jsx';
+import Card from './Card.jsx';
+import Button from './Button.jsx';
+
+function App() {
+    
+    return(
+      <>
+      <Card/>
+      <br/>
+      <Button/>
+      <Header/>
+      <Food/>
+      <Footer/>
+      </>
+    );
+
+}
+
+// video: https://www.youtube.com/watch?app=desktop&v=CgkZ7MvWUAA&ab_channel=BroCode T: 35:59
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,14 @@
+import profilPic from './assets/pb.png'
+
+function Card(){
+    return(
+        <div className="card">
+            <img className="card-image" src={profilPic} alt="profil picture"></img>
+            <h2 className="card-title" >Reiner</h2>
+            <p className="card-text">I learn React and play video games</p>
+
+        </div>
+    );
+};
+
+export default Card
